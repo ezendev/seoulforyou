9678dc5ebfbdc8c6a8adcb91bafc44d717f69349
s2ljeun <s2ljeun@gmail.com>
2022-11-29 17:35:34 +0900

    여행지리스트 mvc 연결 (세션 저장X)

--- src/main/java/com/ezdev/sfy/TourController.java
+++ src/main/java/com/ezdev/sfy/TourController.java
@@ -1,30 +1,28 @@
 package com.ezdev.sfy;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ezdev.sfy.dto.TourDTO;
 import com.ezdev.sfy.service.TourMapper;
 
 @Controller
 public class TourController {
-	//@Autowired
-	//private TourMapper tourMapper;
+	@Autowired
+	private TourMapper tourMapper;
 	
 	//여행지
 	@RequestMapping("/tourList.do")
 	public String tourList(HttpServletRequest req) {
-		HttpSession session = req.getSession();
+		List<TourDTO> list = tourMapper.listTour();
 		
-		TourMapper tlist = (TourMapper)session.getAttribute("tourList");
-		if(tlist == null) {
-			tlist = new TourMapper();
-		}
-		
-		tlist.selectAllTour("all");
-		session.setAttribute("tourList", tlist);
+		req.setAttribute("listTour", list);
 		
 		return "tour/tourList";
 	}
--- src/main/java/com/ezdev/sfy/service/TourMapper.java
+++ src/main/java/com/ezdev/sfy/service/TourMapper.java
@@ -1,6 +1,5 @@
 package com.ezdev.sfy.service;
 
-import java.util.Hashtable;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -13,25 +12,11 @@ import com.ezdev.sfy.dto.TourDTO;
 public class TourMapper {
 	@Autowired
 	private SqlSession sqlSession;
-	
-	Hashtable<String, List<TourDTO>> ht;
-	
-	public TourMapper() {
-		ht = new Hashtable<>();
-	}
-	
-	public Hashtable<String, List<TourDTO>> getHt(){
-		return ht;
-	}
 
-	public void selectAllTour(String all) {
-		if (ht.containsKey(all)) {
-			return;
-		}
-		List<TourDTO> list = sqlSession.selectList("selectAllTour");
-		ht.put(all, list);
-		
+	public List<TourDTO> listTour() {
+		return sqlSession.selectList("listTour");
 	}
+	
 	
 	
 
