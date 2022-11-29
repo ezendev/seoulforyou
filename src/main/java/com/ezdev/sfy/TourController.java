b0b0d30bb1780e765a1d7ba369484f26ee3081ac
s2ljeun <s2ljeun@gmail.com>
2022-11-29 17:10:03 +0900

    여행지리스트 mvc 연결 및 select 쿼리 작성| TourController, TourMapper, tourMapper.xml

--- src/main/java/com/ezdev/sfy/TourController.java
+++ src/main/java/com/ezdev/sfy/TourController.java
@@ -1,22 +1,32 @@
 package com.ezdev.sfy;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ezdev.sfy.service.TourMapper;
+
 @Controller
 public class TourController {
+	//@Autowired
+	//private TourMapper tourMapper;
 	
 	//여행지
 	@RequestMapping("/tourList.do")
-	public String tour() {
+	public String tourList(HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		
+		TourMapper tlist = (TourMapper)session.getAttribute("tourList");
+		if(tlist == null) {
+			tlist = new TourMapper();
+		}
+		
+		tlist.selectAllTour("all");
+		session.setAttribute("tourList", tlist);
+		
 		return "tour/tourList";
 	}
 
-	/*
-	//여행지 - 상세보기
-	@RequestMapping("/tourView.do")
-	public String selected_travel() {
-		return "tour/tourView";
-	}
-	*/
 }
