0243640b73ea0da506c9db1cd2629a2736109850
Jiho you <youkkkwww@naver.com>
2022-12-01 14:09:24 +0900

    1.tourMapper.xml-select문 추가 2.tourMapper.java-listTourByRegion 메서드 추가 3.index.jsp-href="tourList.do?region 추가 4.TourController- if(region != null) { 		         List<TourDTO> list = tourMapper.listTourByRegion(region); 		         session.setAttribute("tourList", list); 		      } 		return "tour/tourList"; 추가

--- src/main/java/com/ezdev/sfy/TourController.java
+++ src/main/java/com/ezdev/sfy/TourController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ezdev.sfy.dto.TourDTO;
 import com.ezdev.sfy.service.TourMapper;
@@ -16,19 +17,24 @@ import com.ezdev.sfy.service.TourMapper;
 public class TourController {
 	@Autowired
 	private TourMapper tourMapper;
-	
 	//여행지
 	@RequestMapping("/tourList.do")
-	public String tourList(HttpServletRequest req) {
+	public String tourList(HttpServletRequest req,@RequestParam(required=false) String region) {
 		HttpSession session = req.getSession();
+		session.removeAttribute("tourList");
 		
 		//세션에 "tourList" 키의 여행지 목록이 없다면 새로 만들어 저장한다
 		if(session.getAttribute("tourList") == null) {
 			List<TourDTO> list = tourMapper.listTour();
-			session.setAttribute("tourList", list);
+			session.setAttribute("tourList", list);			
 		}
-		
+		   if(region != null) {
+		         List<TourDTO> list = tourMapper.listTourByRegion(region);
+		         session.setAttribute("tourList", list);
+		      }
 		return "tour/tourList";
 	}
+	   
+	
 
 }
