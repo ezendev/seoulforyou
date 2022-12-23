20bc0cc68cd47d689a12cc07e0fa1c4635e66c81
Jiho you <youkkkwww@naver.com>
2022-12-23 12:18:46 +0900

    지도 페이징

--- src/main/java/com/ezdev/sfy/service/TourMapper.java
+++ src/main/java/com/ezdev/sfy/service/TourMapper.java
@@ -30,15 +30,17 @@ public class TourMapper {
       return sqlSession.selectList("listTour", map);
    }
    
-   public List<TourDTO> listTourByRegion(String region) {
-	   Map<String, String> map = new HashMap<>();
+   public List<TourDTO> listTourByRegion(String region, int startRow, int endRow) {
+	   Map<String, Object> map = new HashMap<>();
 	      String [] str=new String[] {"강서구","양천구","구로구","영등포구","금천구","동작구","관악구","서초구","강남구","송파구","강동구","마포구","용산구","성동구","광진구","서대문구","종로구","중구","동대문구","은평구","성북구","중랑구","강북구","도봉구","노원구"};
 	      for (int i = 1; i <=str.length; i++) {	
 				if(region.equals(Integer.toString(i))) {
 					  map.put("region", str[i-1]);
 				}
 				 
-			}		 
+			}
+	      map.put("start", startRow);
+		  map.put("end", endRow);
 	      return sqlSession.selectList("listTourByRegion", map);
    }
    
@@ -49,6 +51,22 @@ public class TourMapper {
 			map.put("end", endRow);
 		
 		return sqlSession.selectList("listTourByType", map);
+	}
+   
+	public List<TourDTO> listTourByRegionType(String tourType, String region, int startRow, int endRow) {
+		Map<String, Object> map = new HashMap<>();
+		String [] str=new String[] {"강서구","양천구","구로구","영등포구","금천구","동작구","관악구","서초구","강남구","송파구","강동구","마포구","용산구","성동구","광진구","서대문구","종로구","중구","동대문구","은평구","성북구","중랑구","강북구","도봉구","노원구"};
+	      for (int i = 1; i <=str.length; i++) {	
+				if(region.equals(Integer.toString(i))) {
+					  map.put("region", str[i-1]);
+				}
+				 
+			}
+		map.put("type", Integer.parseInt(tourType));
+		map.put("start", startRow);
+		map.put("end", endRow);
+	
+	return sqlSession.selectList("listTourByRegionType", map);
 	}
    
    public int getTourCount() {
